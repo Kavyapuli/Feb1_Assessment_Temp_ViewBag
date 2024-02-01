c70e44411888736a83d17ad5aa75710df6af934b
Kavyapuli <pulikavya10@gmail.com>
2024-02-01 16:03:22 +0530

    Add project files.

---
+++ Feb1_Assessment/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feb1_Assessment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>()
+            {
+                "kavya",
+                "Sweety"
+            };
+            ViewBag.namelist = list;
+            return View();
+        }
+        public ActionResult ReDirectDemo()
+        {
+            TempData["tempmsg"] = "Welcome to our page";
+            return RedirectToAction("DisplayMessage");
+        }
+        public ActionResult DisplayMessage()
+        {
+            
+            return View();
+        }
+    }
+}
